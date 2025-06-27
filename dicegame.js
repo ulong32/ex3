6d5d51cd0983a19a535898e73db332716ba23d35
ulong32 <64057266+ulong32@users.noreply.github.com>
2025-06-27 11:23:46 +0900

    Greetings added

--- dicegame.js
+++ dicegame.js
@@ -1,10 +1,15 @@
 function dice() {
     return Math.floor(Math.random() * 6 + 1);
 }
-
-console.log("Rolling dice...");
-let d1=dice();
-console.log("Die 1:" + d1);
-let d2=dice();
-console.log("Die 2: " + d2);
-console.log("Total value: " + d1+d2);
+function main(input){
+    console.log("What is your name?");
+    let name = input.shift().trim();
+    console.log("Hello " + name + "!");
+    console.log("Rolling dice...");
+    let d1=dice();
+    console.log("Die 1:" + d1);
+    let d2=dice();
+    console.log("Die 2: " + d2);
+    console.log("Total value: " + d1+d2);
+}
+main(require('fs').readFileSync('/dev/stdin', 'utf8').split('\n'));
