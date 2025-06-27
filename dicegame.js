771fc1dd2403ae807736662ea0c39c907ef83e1c
ulong32 <64057266+ulong32@users.noreply.github.com>
2025-06-27 11:18:34 +0900

    Dice game: initial revision

---
+++ dicegame.js
@@ -0,0 +1,10 @@
+function dice() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+console.log("Rolling dice...");
+let d1=dice();
+console.log("Die 1:" + d1);
+let d2=dice();
+console.log("Die 2: " + d2);
+console.log("Total value: " + d1+d2);
