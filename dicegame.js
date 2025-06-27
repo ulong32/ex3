84b1cc4147ecd80a5dc00610303c8d2333849fad
ulong32 <64057266+ulong32@users.noreply.github.com>
2025-06-27 11:26:57 +0900

    Won/lost message added

--- dicegame.js
+++ dicegame.js
@@ -8,3 +8,8 @@ console.log("Die 1:" + d1);
 let d2=dice();
 console.log("Die 2: " + d2);
 console.log("Total value: " + d1+d2);
+if(d1 + d2 <= 6) {
+    console.log("You lose!");
+} else {
+    console.log("You win!");
+}
